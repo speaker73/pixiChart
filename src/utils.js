d79a41cf65fd7efe14e0583e89af59d37531f17b
VMelnychuk <vladyslav.melnychuk@onseo.biz>
2019-03-26 11:17:23 +0200

    calcY algoritm change

--- src/utils.js
+++ src/utils.js
@@ -1,27 +1,10 @@
-/*export function calcY([x1, y1, x2, y2], x) {
-    const k = ((y2 + y1) / (1 + x1 / x2)) / x2;
-    const m = k * x1 - y1;
-
-    return {
-        x,
-        y: k * x + m
-    }
-};
-*/
-export function calcY(data, x3) {
-    const [x1, y1, x2, y2] = data;
-    const a = y1 - y2;
-    const b = x2 - x1;
-    const rad = Math.atan(a / b);
-    // const beta = (90 *  Math.PI/180)-rad;
-    const ka = x3 - x1;
-    const kb = ka * Math.tan(rad);
-    const res = a + y2 - kb;
-    //console.log({res, ka,kb, a, b});
-    return {
-    	x:x3, 
-    	y:res
-    }
+export function calcY([x1, y1, x2, y2], x){
+  var m = (y2-y1)/(x1-x2);
+  var k = m*x1 + y1;
+  return {
+  	y: k - m*x ,
+  	x
+  }
 }
 
 export function findColum(columnName) {
--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,12 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
-  mode:'development',
-  
+  mode:'production',
+   optimization:{
+  	 minimize: true,
+  },
   output: {
-    filename: 'graph.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   watch: true,
